3a5c87796e0967fb0ba1e5710a5d7fab908e5452
Jason Arnold <jason.arnold.00@gmail.com>
2016-06-22 17:16:56 +0000

    add header and footer ejs, add body-parser, add css file

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var express = require("express");
-var app = express();
-var request = require("request");
+var express = require("express"),
+    app = express(),
+    request = require("request"),
+    bodyParser = require("body-parser");
+
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
     res.render("search");
@@ -19,7 +23,6 @@ app.get("/results", function(req, res){
 });
 
 app.get("/details", function(req, res){
-    console.log(res.app);
 });
 
 app.listen (process.env.PORT, process.env.IP, function(){
